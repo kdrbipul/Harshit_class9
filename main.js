d66021257d5436eff5c149bd455133b7a45b5469
kdrbipul <kdrbipul@gmail.com>
2023-09-28 02:11:00 +0600

    second commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// primitive data type
+let num1 = 6;
+let num2 = num1;
+console.log(num1);
+console.log(num2);
+num1++
+console.log(num1);
+console.log(num2);
+
+// reference data type
+
+const array1 = ['banana','mango'];
+const array2 = array1;
+console.log(array1);
+console.log(array2);
+array1.push('orange')
+console.log(array1);
+console.log(array2);
+
